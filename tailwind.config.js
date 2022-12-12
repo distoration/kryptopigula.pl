9bc4828878e68467363d208a60a7a5915d970f7e
Dist <40737779+DorianSuffer@users.noreply.github.com>
2022-12-12 18:36:51 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html", "./build/js/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        papayawhip: {
+          light: '#fef4e4',
+          DEFAULT: '#ffefd5',
+          dark: '#fee5bc',
+        },
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'pink': '#ff49db',
+        'orange': '#ff7849',
+        'green': '#13ce66',
+        'yellow': '#ffc82c',
+        'gray-dark': '#273444',
+        'gray': '#8492a6',
+        'gray-light': '#d3dce6',
+      },
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)' },
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
+    },
+  },
+  plugins: [],
+}
